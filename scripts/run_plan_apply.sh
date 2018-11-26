7742f1737fc1ef2996de136240a9bca041eb5ae0
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-11-26 15:50:27 +0100

    Update run_plan_apply.sh

--- scripts/run_plan_apply.sh
+++ scripts/run_plan_apply.sh
@@ -4,37 +4,38 @@
 # The organization name is passed as a first argument to the script
 # The workspace name is passaed as a second argument to the script
 
+TFE=https://app.terraform.io
 
 # The following block of code is going to retrieve your TFE Workspace ID
-WSPACE_ID="$(curl \
-  --header "Authorization: Bearer $TOKEN" \
+WSPACE="$(curl \
+  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/vnd.api+json" \
-  https://app.terraform.io/api/v2/organizations/"$1"/workspaces/"$2" 2>/dev/null | jq '.data | .id' | tr -d \")"
+  ${TFE}/api/v2/organizations/${1}/workspaces/"{2} 2>/dev/null | jq '.data | .id' | tr -d \")"
 
 # The following is added for debugging purposes. Will be removed in the final version
-echo -e "Your workspace ID is: \n $WSPACE_ID \n"
+echo -e "Your workspace ID is: \n ${WSPACE} \n"
 
 # The following block of code is going to retrieve the current run ID of your TFE Workspace
-CURRENT_RUN_ID="$(curl \
-  --header "Authorization: Bearer $TOKEN" \
+CURRENT_RUN="$(curl \
+  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/vnd.api+json" \
-  https://app.terraform.io/api/v2/workspaces/"$WSPACE_ID"/runs 2>/dev/null | jq '.data | .[0] | .id' | tr -d \")"
+  ${TFE}/api/v2/workspaces/${WSPACE}/runs 2>/dev/null | jq '.data | .[0] | .id' | tr -d \")"
 
 
 # The following is added for debugging purposes. Will be removed in the final version
 
-echo -e "Your Current Run ID is: \n $CURRENT_RUN_ID \n"
+echo -e "Your Current Run ID is: \n ${CURRENT_RUN} \n"
 
 # The following block of code is going to retrieve the configuration ID of the current run on your TFE Workspace
 
-CONF_ID="$(curl \
-  --header "Authorization: Bearer $TOKEN" \
+CONF="$(curl \
+  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/vnd.api+json" \
-  https://app.terraform.io/api/v2/runs/"$CURRENT_RUN_ID"/configuration-version 2>/dev/null | jq '.data | .id' | tr -d \")"
+  ${TFE}/api/v2/runs/${CURRENT_RUN}/configuration-version 2>/dev/null | jq '.data | .id' | tr -d \")"
 
 
 # The following is added for debugging purposes. Will be removed in the final version
-echo -e "Your Configuration ID is: \n $CONF_ID \n"
+echo -e "Your Configuration ID is: \n ${CONF} \n"
 
 
 # Create a payload file
@@ -42,12 +43,12 @@ echo -e "Your Configuration ID is: \n $CONF_ID \n"
 INPUT_FILE="/vagrant/templates/payload_template"
 OUTPUT_FILE="/tmp/payload.json"
 
-cat $INPUT_FILE | sed "s/WSPACE_ID/"$WSPACE_ID"/g; s/CONF_ID/"$CONF_ID"/g" > $OUTPUT_FILE
+cat $INPUT_FILE | sed "s/WSPACE/"${WSPACE}"/g; s/CONF/"${CONF}"/g" > ${OUTPUT_FILE}
 
 # The following block of code is for debugging purposes only, it will be removed in the final version
-#cat $OUTPUT_FILE | grep "$WSPACE_ID" > /dev/null
+#cat ${OUTPUT_FILE} | grep "${WSPACE}" > /dev/null
 
-if cat $OUTPUT_FILE | grep "$WSPACE_ID" > /dev/null; then
+if cat ${OUTPUT_FILE} | grep "${WSPACE}" > /dev/null; then
     echo -e "SUCCESS: Your payload file was created \n"
 else
     echo -e "FAILURE: Something went wrong \n"
@@ -56,35 +57,32 @@ fi
 # The following block of code is going to create a new run within your TFE Workspace
 
 curl \
-  --header "Authorization: Bearer $TOKEN" \
+  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
-  --data @$OUTPUT_FILE \
-  https://app.terraform.io/api/v2/runs 2>/dev/null | jq '.data | .id' | tr -d \" > /tmp/new_tfe_run_id
+  --data @${OUTPUT_FILE} \
+  ${TFE}/api/v2/runs 2>/dev/null | jq '.data | .id' | tr -d \" > /tmp/new_tfe_run_id
 
 NEW_TFE_RUN="$(cat /tmp/new_tfe_run_id)"
 
 # The following is added for debugging purposes. Will be removed in the final version
-echo -e "Your New Run ID is: \n "$NEW_TFE_RUN" \n"
+echo -e "Your New Run ID is: \n "${NEW_TFE_RUN}" \n"
 
 # The following block of code will apply your last created run
 
 # NOTE!!! There should be some check to verify that the previous operation (plan) has ended. 
 # Otherwise the apply operation will not run.
 
-sleep 10
-echo 'waiting...'
-sleep 10
-echo 'waiting...'
-sleep 10
-echo 'waiting...'
-sleep 10
+echo "waiting 1 minute for the plan to finish
+for x in {1..30}; do
+  echo -n .
+  sleep 2
 echo 'Running the apply block'
 
 curl \
-  --header "Authorization: Bearer $TOKEN" \
+  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
-  --data @$OUTPUT_FILE \
-  https://app.terraform.io/api/v2/runs/$NEW_TFE_RUN/actions/apply
+  --data @${OUTPUT_FILE} \
+  ${TFE}/api/v2/runs/${NEW_TFE_RUN}/actions/apply
 
