c64b50db801e840be617ec772abd52b016041db7
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-11-26 16:28:45 +0200

    Added a block of code which triggers an apply action in TFE

--- scripts/run_plan_apply.sh
+++ scripts/run_plan_apply.sh
@@ -59,11 +59,32 @@ curl \
   --header "Authorization: Bearer $TOKEN" \
   --header "Content-Type: application/vnd.api+json" \
   --request POST \
-  --data @/tmp/payload.json \
+  --data @$OUTPUT_FILE \
   https://app.terraform.io/api/v2/runs 2>/dev/null | jq '.data | .id' | tr -d \" > /tmp/new_tfe_run_id
 
 NEW_TFE_RUN="$(cat /tmp/new_tfe_run_id)"
+
 # The following is added for debugging purposes. Will be removed in the final version
 echo -e "Your New Run ID is: \n "$NEW_TFE_RUN" \n"
 
+# The following block of code will apply your last created run
+
+# NOTE!!! There should be some check to verify that the previous operation (plan) has ended. 
+# Otherwise the apply operation will not run.
+
+sleep 10
+echo 'waiting...'
+sleep 10
+echo 'waiting...'
+sleep 10
+echo 'waiting...'
+sleep 10
+echo 'Running the apply block'
+
+curl \
+  --header "Authorization: Bearer $TOKEN" \
+  --header "Content-Type: application/vnd.api+json" \
+  --request POST \
+  --data @$OUTPUT_FILE \
+  https://app.terraform.io/api/v2/runs/$NEW_TFE_RUN/actions/apply
 
