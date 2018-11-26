aca3ce3de9d22c5288b77f614827204e9bd3bb06
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-11-26 15:27:07 +0200

    Added a line that creates a new run in your TFE Workspace

--- scripts/run_plan_apply.sh
+++ scripts/run_plan_apply.sh
@@ -25,7 +25,7 @@ CURRENT_RUN_ID="$(curl \
 
 echo -e "Your Current Run ID is: \n $CURRENT_RUN_ID \n"
 
-# The following block of code is going to retrieve the configuration ID of the current run on your TFE Workspace 
+# The following block of code is going to retrieve the configuration ID of the current run on your TFE Workspace
 
 CONF_ID="$(curl \
   --header "Authorization: Bearer $TOKEN" \
@@ -43,3 +43,27 @@ INPUT_FILE="/vagrant/templates/payload_template"
 OUTPUT_FILE="/tmp/payload.json"
 
 cat $INPUT_FILE | sed "s/WSPACE_ID/"$WSPACE_ID"/g; s/CONF_ID/"$CONF_ID"/g" > $OUTPUT_FILE
+
+# The following block of code is for debugging purposes only, it will be removed in the final version
+#cat $OUTPUT_FILE | grep "$WSPACE_ID" > /dev/null
+
+if cat $OUTPUT_FILE | grep "$WSPACE_ID" > /dev/null; then
+    echo -e "SUCCESS: Your payload file was created \n"
+else
+    echo -e "FAILURE: Something went wrong \n"
+fi
+
+# The following block of code is going to create a new run within your TFE Workspace
+
+curl \
+  --header "Authorization: Bearer $TOKEN" \
+  --header "Content-Type: application/vnd.api+json" \
+  --request POST \
+  --data @/tmp/payload.json \
+  https://app.terraform.io/api/v2/runs 2>/dev/null | jq '.data | .id' | tr -d \" > /tmp/new_tfe_run_id
+
+NEW_TFE_RUN="$(cat /tmp/new_tfe_run_id)"
+# The following is added for debugging purposes. Will be removed in the final version
+echo -e "Your New Run ID is: \n "$NEW_TFE_RUN" \n"
+
+
